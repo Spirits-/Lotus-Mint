2871c17bbb81edf7c31159e3c8287816b49485f4
GitHub Action <action@github.com>
2022-05-22 10:15:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/spirits/lotusmint/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/spirits/lotusmint/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.spirits.lotusmint.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.spirits.lotusmint.MyBundle
 
 class MyApplicationService {
 
